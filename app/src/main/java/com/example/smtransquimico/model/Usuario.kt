4bb0defe3892736c036c2f82db49b95b4904b050
matheus.p <matheus.p@ambevtech.com.br>
2024-08-22 17:57:58 -0300

    Removendo classe não usadas e renomeadas

--- app/src/main/java/com/example/smtransquimico/model/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package com.example.smtransquimico.model
-
-data class Usuario(
-    var userId: String = "",
-    val userName: String = "",
-    val profileImage: String = "",
-    val phone: String = "",
-    val email: String = ""
-)
